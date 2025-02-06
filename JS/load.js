026689e8b8f3f63f394ca5d4edceaa70bd5b118a
PsIx52 <168657951+PsIx52@users.noreply.github.com>
2025-02-06 23:39:58 +0300

    Update load.js

--- JS/load.js
+++ JS/load.js
@@ -1,8 +1,10 @@
+console.log("load.js загружен");
+
 import { setActiveButton } from './shop.js'; // Импортируем функцию
 
 const shopButton = document.getElementById('shop-button');
 if (shopButton) {
-        fetch('HTML/shop.html') // URL файла HTML, который нужно загрузить
+        fetch('./HTML/shop.html') // URL файла HTML, который нужно загрузить
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Сетевая ошибка: ответ не был успешным');
@@ -24,7 +26,7 @@ if (shopButton) {
 const friendButton = document.getElementById('friend-button');
 if (friendButton) {
     friendButton.addEventListener('click', function() {
-        fetch('HTML/ref.html') // URL файла HTML, который нужно загрузить
+        fetch('./HTML/ref.html') // URL файла HTML, который нужно загрузить
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Сетевая ошибка: ответ не был успешным');
@@ -43,7 +45,7 @@ if (friendButton) {
 const profileButton = document.getElementById('profile-button');
 if (profileButton) {
     profileButton.addEventListener('click', function() {
-        fetch('HTML/profile.html') // URL файла HTML, который нужно загрузить
+        fetch('./HTML/profile.html') // URL файла HTML, который нужно загрузить
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Сетевая ошибка: ответ не был успешным');
